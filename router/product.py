139fc2ffb551df4f3319b1237bf28fd85d3aa0ea
Chi-Shan Yu <chisamyu@seed.net.tw>
2022-10-04 11:46:00 +0800

    first commit

---
+++ router/product.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter
+import time
+from db.productJson import product_list
+
+router = APIRouter(
+    prefix='/products',
+    tags=['products']
+)
+
+
+@router.get('/')
+def get_all_products():
+    # return products
+    # time.sleep(1)
+    return product_list
+
+
+@router.get('/id/{product_id}')
+def get_product_by_id(product_id):
+    # time.sleep(1)
+    return next((product for product in product_list if product['id'] == product_id))
+
+
+@router.get("/{category}")
+def get_product_by_category(category):
+    category_list = []
+    # time.sleep(1)
+
+    for product in product_list:
+        if product['category'].upper() == category.upper():
+            category_list.append(product)
+    return category_list
