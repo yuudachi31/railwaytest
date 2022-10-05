374d2e1a045462f926e9fb1faafffac6fa90b8e2
Chi-Shan Yu <chisamyu@seed.net.tw>
2022-10-05 09:36:27 +0800

    add port 5500

--- main.py
+++ main.py
@@ -31,6 +31,8 @@ origins = [
     'http://localhost:5173',
     'http://127.0.0.1:8080',
     'http://localhost:8080',
+    'http://127.0.0.1:5500',
+    'http://localhost:5500',
     'https://react4railway2022-production.up.railway.app'
 ]
 
