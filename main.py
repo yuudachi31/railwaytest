08115cd7d74223031f8d294e23afaa266651b1b6
Chi-Shan Yu <chisamyu@seed.net.tw>
2022-10-05 09:23:21 +0800

    add localhost with port 8080 origin

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ origins = [
     'http://localhost:3000',
     'http://127.0.0.1:5173',
     'http://localhost:5173',
+    'http://127.0.0.1:8080',
+    'http://localhost:8080',
     'https://react4railway2022-production.up.railway.app'
 ]
 
