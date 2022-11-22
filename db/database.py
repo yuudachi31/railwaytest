8e9483d649932603ce81dad82ddf2031666718cb
Chi-Shan Yu <chisamyu@seed.net.tw>
2022-11-22 13:20:16 +0800

    change to postgresql

--- db/database.py
+++ db/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./shopping-cart.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./shopping-cart.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:u55mmQ4qPj4vTdCx5xPl@containers-us-west-89.railway.app:6663/railway"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
