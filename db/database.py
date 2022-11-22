1e0692697a64aa0e7a7c31d2592f6b472ac3f02f
Chi-Shan Yu <chisamyu@seed.net.tw>
2022-11-22 16:07:57 +0800

    add psycopg2

--- db/database.py
+++ db/database.py
@@ -2,14 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./shopping-cart.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:u55mmQ4qPj4vTdCx5xPl@containers-us-west-89.railway.app:6663/railway"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./shopping-cart.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:u55mmQ4qPj4vTdCx5xPl@containers-us-west-89.railway.app:6663/railway"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-) 
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# ) 
 
-# engine = create_engine(SQLALCHEMY_DATABASE_URL) 
+engine = create_engine(SQLALCHEMY_DATABASE_URL) 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
