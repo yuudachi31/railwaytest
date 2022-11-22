01e89857d8822831ff9db88b562341967f679c07
Chi-Shan Yu <chisamyu@seed.net.tw>
2022-11-22 10:13:49 +0800

    remove localhost CORS

--- main.py
+++ main.py
@@ -25,14 +25,6 @@ if __name__ == "__main__":
     uvicorn.run("main:app", port=5000, reload=True)
 
 origins = [
-    'http://127.0.0.1:3000',
-    'http://localhost:3000',
-    'http://127.0.0.1:5173',
-    'http://localhost:5173',
-    'http://127.0.0.1:8080',
-    'http://localhost:8080',
-    'http://127.0.0.1:5500',
-    'http://localhost:5500',
     'https://react4railway2022-production.up.railway.app'
 ]
 
